fbd3b2dda76938c10fae9aae9d9fda9acfad72eb
Marc Hull <aozine@gmail.com>
2022-07-31 16:43:08 +0100

    Improve setup.py and setup.cfg.

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-from distutils.core import setup
+
+from setuptools import setup
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 setup(
-  name = 'cmdfns',
-  packages = ['cmdfns'],
+  name="cmdfns",
+  packages=["cmdfns"],
   package_data={"cmdfns": ["py.typed"]},
-  version = '0.1',
-  license = 'MIT',
-  description = 'Make Python functions callable from the command-line',
-  author = 'Marc Hull',
-  author_email = 'aozine@gmail.com',
-  url = 'https://github.com/aozine/cmdfns',
-  download_url = 'https://github.com/Aozine/cmdfns/archive/refs/tags/v0.1.0.tar.gz',
-  keywords = ['command-line'],
+  version="0.3.0",
+  license="MIT",
+  description="Make Python functions callable from the command-line",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author="Marc Hull",
+  author_email="aozine@gmail.com",
+  url="https://github.com/aozine/cmdfns",
+  keywords=["command-line"],
+  python_requires=">=3.7",
   install_requires=[],
   classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.10',
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
   ],
 )
---
+++ upload.bat
@@ -0,0 +1,3 @@
+@echo off
+python setup.py sdist
+twine upload dist/*
